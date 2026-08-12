c117252914ac0f616d6cc7b8c7c3785eb1c377a3
JioGramedia <140771798+muridbiasa@users.noreply.github.com>
2026-08-13 00:27:04 +0700

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -7,18 +7,29 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: [
-              "script-src 'self'",
-              "https://snap-assets.midtrans.com",
-              "https://api.midtrans.com",
-              "https://pay.google.com",
-              "https://gwk.gopayapi.com",
-              "https://www.googletagmanager.com",
-              "https://o.alicdn.com",
-              "https://g.alicdn.com",
-              "'unsafe-inline'",
-              "'unsafe-eval'",
-            ].join(' '),
+            value: `
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' 
+              https://app.midtrans.com 
+              https://snap-assets.midtrans.com 
+              https://api.midtrans.com 
+              https://pay.google.com 
+              https://gwk.gopayapi.com 
+              https://www.googletagmanager.com 
+              https://o.alicdn.com 
+              https://g.alicdn.com;
+              
+              connect-src 'self' 
+              https://app.midtrans.com 
+              https://api.midtrans.com 
+              https://snap-assets.midtrans.com;
+              
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' data: https: blob:;
+              font-src 'self' data:;
+              frame-src 'self' https://app.midtrans.com https://pay.google.com;
+              base-uri 'self';
+              object-src 'none';
+            `.replace(/\s+/g, ' ').trim(),
           },
           {
             key: 'X-Content-Type-Options',
